da8ebcb042c597f3410a082efa9b1d1e961768f9
tjoeunJava <dlrkdms5323@naver.com>
2025-03-25 17:24:49 +0900

    로그인 부분 수정 완료

--- src/main/java/com/tjoeun/mapper/UserMapper.java
+++ src/main/java/com/tjoeun/mapper/UserMapper.java
@@ -14,17 +14,9 @@ public interface UserMapper {
 	String checkUserId(String id);
 
 	// 회원 가입하기
-	//                                (1, '더조은', 'tjoeun', '1234')
-	//@Insert("INSERT INTO user_table VALUES(user_seq.nextval, #{user_name}, #{user_id}, #{user_pw})")
 	@Insert("INSERT INTO tjoeun.user(name, id, password, username, role) VALUES(#{name}, #{id}, #{password}, #{username}, #{role})")
-	//insert into tjoeun.user(name, id, password, username, role) 
-	//values(‘홍길동’, ‘honggildong’, ‘hong1234’, ‘hong’, 0);
 	void addUserInfo(UserDTO joinUserDTO);
 	
-	// 로그인 성공했을 때, 회원 정보 가져오기
-	//  ㄴ 가져와서, Spring Framework 가  Session Scope 에
-	//     서버 실행할 때 자동으로 생성해 놓은 UserDTO 에 
-	//     로그인한 회원의 정보를 위함 
 	@Select("SELECT * " +
 	        "FROM user " +
 	        "WHERE id = #{id} AND password = ${password}")
