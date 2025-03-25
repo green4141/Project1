013c68846368e468d0ff400bcc37aebbc2312295
tjoeunJava <dud-0823@naver.com>
2025-03-25 16:09:23 +0900

    board 목록 보기

--- src/main/java/com/tjoeun/mapper/BoardMapper.java
+++ src/main/java/com/tjoeun/mapper/BoardMapper.java
@@ -18,12 +18,9 @@ public interface BoardMapper {
 					"WHERE board_id=#{board_id}")
 	String getBoardInfoName(int board_id);
 	
-	@Select("SELECT b.idx, b.title, u.name writer_name, "+
-		  		"to_char(b.date, 'YYYY-MM-DD') date "+
-		  		"FROM board b, user u "+
-		  		"WHERE b.user = u.idx "+
-		  		"AND b.board_id = #{board_id} "+
-		  		"ORDER BY b.idx desc")	
+	@Select("SELECT * FROM v_board_user "
+			+ "WHERE board_id = #{board_id} "
+			+ "ORDER BY idx DESC")
 	List<BoardDTO> getBoardList(int board_id);
 
 }
